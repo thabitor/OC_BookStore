4dfc80e7d1df89606ff2f19153cf836407a147a2
thabitor <thabithamror@gmail.com>
2022-06-18 11:33:36 +0200

    Work in progress

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 // Elements retrieved
-const body = document.getElementsByTagName('body')[0];
-const myBooks = document.getElementById('myBooks');
-const content = document.getElementById('content');
-const addBookBtn = document.getElementById('addbook');
+
+const myBooks = document.getElementById("myBooks");
+const content = document.getElementById("content");
+const addBookBtn = document.getElementById("addbook");
 
 // Elements created
-const searchContainer = document.createElement('div');
-const searchBtnContainer = document.createElement('div');
-const searchForm = document.createElement('form');
-const titleInput = document.createElement('input');
-const authorInput = document.createElement('input');
-const searchResultsBlock = document.createElement('div');
-const titleResults = document.createElement('h2');
+const searchContainer = document.createElement("div");
+const searchBtnsContainer = document.createElement("div");
+const searchForm = document.createElement("form");
+const titleInput = document.createElement("input");
+titleInput.setAttribute("type", "text");
+const authorInput = document.createElement("input");
+const titleResults = document.createElement("h2");
 titleResults.innerHTML = "Search Results";
-
-searchContainer.setAttribute('class', 'container');
-searchForm.setAttribute('class', 'form-group');
+authorInput.setAttribute("type", "text");
+searchContainer.setAttribute("class", "container");
+searchForm.setAttribute("class", "form-group");
 
 // // Buttons
-const searchBtn = document.createElement('button');
-const cancelBtn = document.createElement('button');
-searchBtn.innerHTML = "Search"
-cancelBtn.innerHTML = "Cancel"
+const searchBtn = document.createElement("button");
+const cancelBtn = document.createElement("button");
+searchBtn.innerHTML = "Search";
+cancelBtn.innerHTML = "Cancel";
 
-addBookBtn.addEventListener('click', ()=> {
-
-    myBooks.removeChild(content);
-    body.appendChild(searchContainer);
-    searchContainer.appendChild(searchForm);
-    searchForm.appendChild(titleInput);
-    searchForm.appendChild(authorInput);
-    searchForm.appendChild(searchBtnContainer);
-    searchBtnContainer.appendChild(searchBtn);
-    searchBtnContainer.appendChild(cancelBtn);
-
+addBookBtn.addEventListener("click", () => {
+  console.log(myBooks.children);
+  console.log(document.body.children);
+  myBooks.removeChild(content);
+  myBooks.appendChild(searchContainer);
+  searchContainer.appendChild(searchForm);
+  searchForm.appendChild(titleInput);
+  searchForm.appendChild(authorInput);
+  searchForm.appendChild(searchBtnsContainer);
+  searchBtnsContainer.appendChild(searchBtn);
+  searchBtnsContainer.appendChild(cancelBtn);
 });
 
-searchBtn.addEventListener('click', () => {
-    
-    myBooks.removeChild(content);
-    body.appendChild(searchResultsBlock);
-    searchResultsBlock.appendChild(titleResults);
+searchBtn.addEventListener("click", () => {
+  const searchResultsBlock = document.createElement("div");
 
+  //   myBooks.removeChild(content);
+  document.body.appendChild(searchResultsBlock);
+  searchResultsBlock.appendChild(titleResults);
 });
