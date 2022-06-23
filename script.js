7d3e3feab821b3d8f11b966246b68d84e6f05b35
thabitor <thabithamror@gmail.com>
2022-06-23 13:14:29 +0200

    update code and css

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const template = `<hr>
-<section class="textResults" id="txtSection">
-          <h3>Title :</h3>
-          <span class="s-title"> __title__</span>
-          <h4>ID :</h4>
-          <span class="s-bookid"> __bookid__</span>
-          <h3>Authors :</h3>
-          <span class="s-author"> __author__</span>
-          <h3>Description :</h3>
-          <span class="s-desc"> __description__</span>
-        </section>
-        <section class="imgResults" id="imgSection">
+<div class="book-container" id="bookbox">
+  <section class="textResults" id="txtSection">
+    <h3>Title :</h3>
+    <span class="s-title"> __title__</span>
+    <h4>ID :</h4>
+    <span class="s-bookid"> __bookid__</span>
+    <h3>Authors :</h3>
+    <span class="s-author"> __author__</span>
+    <h3>Description :</h3>
+    <span class="s-desc"> __description__</span>
+  </section>
+  <section class="imgResults" id="imgSection">
           <img src="__src__" />
-        </section>
-         </div>
-      </div>`;
+  </section>
+</div>`;
 
 // Elements retrieved
 
@@ -36,12 +36,8 @@ const labelInputTitle = document.createElement("label");
 const labelInputAuthor = document.createElement("label");
 const searchBtn = document.createElement("button");
 const cancelBtn = document.createElement("button");
-// const resultsContainer = document.createElement("div");
-
-// Attributes setting
 
 searchForm.setAttribute("class", "form-group");
-// searchContainer.setAttribute("class", "block-search");
 titleInput.setAttribute("type", "text");
 titleInput.setAttribute("class", "block-search__inputbox");
 titleInput.setAttribute("id", "titleInput");
@@ -53,7 +49,6 @@ labelInputAuthor.setAttribute("for", "authorInput");
 searchBtnsContainer.setAttribute("class", "block-search-buttons");
 searchBtn.setAttribute("class", "block-search__btn");
 cancelBtn.setAttribute("class", "block-search__btn red");
-// resultsContainer.setAttribute("class", "block-search-results");
 
 // InnerHTML
 
@@ -67,7 +62,6 @@ cancelBtn.innerHTML = "Cancel";
 
 addBookBtn.addEventListener("click", () => {
   mainContainer.removeChild(content);
-  // mainContainer.appendChild(searchContainer);
   searchContainer.appendChild(searchForm);
   searchContainer.appendChild(searchBtnsContainer);
   searchForm.appendChild(labelInputTitle);
@@ -78,11 +72,6 @@ addBookBtn.addEventListener("click", () => {
   searchBtnsContainer.appendChild(cancelBtn);
 });
 
-// searchBtn.addEventListener("click", () => {
-//   mainContainer.appendChild(resultsContainer);
-//   resultsContainer.appendChild(titleResults);
-//   resultsContainer.appendChild(bookBoxHTML);
-// });
 
 const pageBtnsDiv = document.createElement("div");
 pageBtnsDiv.classList.add("page-btns-container");
@@ -95,7 +84,6 @@ prevBtn.innerHTML = "<< back";
 pageBtnsDiv.appendChild(prevBtn);
 pageBtnsDiv.appendChild(nextBtn);
 
-/////////....NEW CODE....//////////
 
 const URLbooks =
   "https://www.googleapis.com/books/v1/volumes?q=__title__+inauthor:__author__&startIndex=__start__&maxResults=15&langResrict=en&key=__apiKey__";
@@ -103,17 +91,11 @@ const URLauth =
   "https://www.googleapis.com/books/v1/volumes?q=+inauthor:__author__&startIndex=__start__&maxResults=15&langResrict=en&key=__apiKey__";
 const apiKey = "AIzaSyCc7mtocCRRD4toqVJrcV0AnVPPD6ca_Rw";
 
-// const titleInputHTML = document.getElementById("titleInput");
-// const authorInputHTML = document.getElementById("authorInput");
-// const buttonHTML = document.getElementById("button");
-// const loadingHTML = document.querySelector("h2");
+
 const textbookBoxHTML = document.getElementById("txtSection");
 const imgbookBoxHTML = document.getElementById("imgSection");
 const authorsHTML = document.getElementsByClassName("s-author");
-// const hrLineTop = document.createElement("hr");
-// const authorPgHeading = document.createElement("h3");
-// authorInputHTML.class = "new-author";
-// hrLineTop.id = "above-author-line";
+
 
 let title;
 let author;
@@ -138,7 +120,6 @@ function displayResults(books) {
   }
 }
 
-// ////....////
 
 function loadData(title, author, start) {
   fetch(
@@ -149,7 +130,6 @@ function loadData(title, author, start) {
   )
     .then((response) => response.json())
     .then((data) => {
-      // loadingHTML.classList.add("hidden");
       console.log(data);
       displayResults(data.items);
 
@@ -164,34 +144,18 @@ function loadData(title, author, start) {
           )
             .then((response) => response.json())
             .then((data) => {
-              // loadingHTML.classList.add("hidden");
-              // authorPgHeading.classList.remove("hidden");
-              // hrLineTop.classList.remove("hidden");
-              // mainContainer.insertBefore(
-              //   authorPgHeading,
-              //   mainContainer.children[1]
-              // );
-              // mainContainer.insertBefore(hrLineTop, mainContainer.children[1]);
               bookBoxHTML.innerHTML = "";
-              // authorPgHeading.innerHTML =
-              //   "Listing by author:" + e.target.innerText;
               displayResults(data.items);
             });
         });
       }
-      // mainContainer.appendChild(pageBtnsDiv);
+      
     });
 }
 
 searchBtn.addEventListener("click", () => {
   resultsContainer.appendChild(bookBoxHTML);
-  // resultsContainer.appendChild(titleResults);
-  // resultsContainer.appendChild(bookBoxHTML
-  // );
   bookBoxHTML.innerHTML = "";
-  // loadingHTML.classList.remove("hidden");
-  // authorPgHeading.classList.add("hidden");
-  // hrLineTop.classList.add("hidden");
   title = titleInput.value;
   author = authorInput.value;
   loadData(title, author, 0);
